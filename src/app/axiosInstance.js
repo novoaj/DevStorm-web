40732e3c90f3991cdf1b55ebdb93a235acade080
Jared Novoa <jnovoa@wisc.edu>
2024-10-09 18:20:14 -0700

    refresh logic working

--- src/app/axiosInstance.js
+++ src/app/axiosInstance.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { fetchCSRFAccess, fetchCSRFToken } from './actions/actions';
 
+let isRefreshing = false;
+let refreshSubscribers = [];
+
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
@@ -8,10 +11,14 @@ const axiosInstance = axios.create({
   },
   withCredentials: true,
 });
+const subscribeTokenRefresh = (cb) => refreshSubscribers.push(cb);
 
+const onTokenRefreshed = (token) => {
+  refreshSubscribers.map(cb => cb(token));
+  refreshSubscribers = [];
+};
 axiosInstance.interceptors.request.use(async (request) => {
   const csrfToken = await fetchCSRFAccess();
-  console.log("intercept request setting header token: ", csrfToken);
   if (csrfToken) {
     request.headers['X-CSRF-TOKEN'] = csrfToken;
   }
@@ -25,24 +32,38 @@ axiosInstance.interceptors.response.use(
     async (error) => {
         const originalRequest = error.config;
         if (error.response?.status === 401 && !originalRequest._retry) {
-            console.log("intercepted 401");
+            if (isRefreshing) {
+                return new Promise((resolve) => {
+                    subscribeTokenRefresh((token) => {
+                        originalRequest.headers['X-CSRF-TOKEN'] = token;
+                        resolve(axiosInstance(originalRequest));
+                    });
+                });
+              }
             originalRequest._retry = true;
+            isRefreshing = true;
             try {
                 const csrfRefreshToken = await fetchCSRFToken();
                 const refreshInstance = axios.create({
                     withCredentials: true,
                     baseURL: process.env.NEXT_PUBLIC_API_URL
                 });
-            refreshInstance.defaults.headers.common['X-CSRF-TOKEN'] = csrfRefreshToken;
-            console.log("refresh endpoint being hit, ", csrfRefreshToken);
-            let response = await refreshInstance.post("/token/refresh", {});
-            console.log(response);
-            console.log("retrying original request...");
-            return axiosInstance(originalRequest);
+                refreshInstance.defaults.headers.common['X-CSRF-TOKEN'] = csrfRefreshToken;
+                await refreshInstance.post("/token/refresh", {});
+
+                // set instance header to new access token
+                const newCsrfAccess = await fetchCSRFAccess();
+                axiosInstance.defaults.headers.common['X-CSRF-TOKEN'] = newCsrfAccess;
+                onTokenRefreshed(newCsrfAccess);
+                
+                originalRequest.headers['X-CSRF-TOKEN'] = newCsrfAccess;
+                return axiosInstance(originalRequest);
             } catch (refreshError) {
                 localStorage.removeItem("isLoggedIn");
                 // window.location.href = "/login";
                 return Promise.reject(refreshError);
+            } finally {
+                isRefreshing = false;
             }
         }
         return Promise.reject(error);
